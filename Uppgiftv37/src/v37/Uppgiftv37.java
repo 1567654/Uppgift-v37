5e5c53923f4727692bc063cc8600741691f3061d
1567654 <luohuanyu3124@gmail.com>
2018-09-18 22:35:33 +0200

    copy pasted 10 of them

--- Uppgiftv37/src/v37/Uppgiftv37.java
+++ Uppgiftv37/src/v37/Uppgiftv37.java
@@ -21,7 +21,70 @@ public class Uppgiftv37 {
 		if (tal2 < min); {
 			tal2 = min;
 		}
-		 
+		
+		int tal3 = input.nextInt();
+		if (tal3 > max); {
+			tal3 = max;
+		}
+		if (tal3 < min); {
+			tal3 = min;
+		}
+		
+		int tal4 = input.nextInt();
+		if (tal4 > max); {
+			tal4 = max;
+		}
+		if (tal4 < min); {
+			tal4 = min;
+		}
+		
+		int tal5 = input.nextInt();
+		if (tal5 > max); {
+			tal5 = max;
+		}
+		if (tal5 < min); {
+			tal5 = min;
+		}
+		
+		int tal6 = input.nextInt();
+		if (tal6 > max); {
+			tal6 = max;
+		}
+		if (tal6 < min); {
+			tal6 = min;
+		}
+		
+		int tal7 = input.nextInt();
+		if (tal7> max); {
+			tal7 = max;
+		}
+		if (tal7 < min); {
+			tal7 = min;
+		}
+		
+		int tal8 = input.nextInt();
+		if (tal8 > max); {
+			tal8 = max;
+		}
+		if (tal8 < min); {
+			tal8 = min;
+		}
+		
+		int tal9 = input.nextInt();
+		if (tal9 > max); {
+			tal9 = max;
+		}
+		if (tal9 < min); {
+			tal9 = min;
+		}
+		
+		int tal10 = input.nextInt();
+		if (tal10 > max); {
+			tal10 = max;
+		}
+		if (tal10 < min); {
+			tal10 = min;
+		}
 		
 		
 		
