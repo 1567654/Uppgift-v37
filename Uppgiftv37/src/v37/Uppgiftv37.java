5637d8313600c0e909aea7b2aac5092157cf8244
1567654 <luohuanyu3124@gmail.com>
2018-09-18 22:19:26 +0200

    början av programmet

---
+++ Uppgiftv37/src/v37/Uppgiftv37.java
@@ -0,0 +1,20 @@
+package v37;
+
+import java.util.Scanner;
+
+public class Uppgiftv37 {
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Skriv in 10 heltal");
+		
+		int tal1 = input.nextInt(); 
+		 
+		
+		
+		
+	}
+
+}
