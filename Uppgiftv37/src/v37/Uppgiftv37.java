a521e0ac64afde0c518cb6ee0f37e18e7e39a8e0
1567654 <luohuanyu3124@gmail.com>
2018-09-18 22:50:15 +0200

    Klar produkt

--- Uppgiftv37/src/v37/Uppgiftv37.java
+++ Uppgiftv37/src/v37/Uppgiftv37.java
@@ -15,79 +15,85 @@ public class Uppgiftv37 {
 		int min = tal1;
 		
 		int tal2 = input.nextInt();
-		if (tal2 > max); {
-			tal2 = max;
+		if (tal2 > max) {
+			max = tal2;
 		}
-		if (tal2 < min); {
-			tal2 = min;
+		if (tal2 < min) {
+			min = tal2;
 		}
 		
 		int tal3 = input.nextInt();
-		if (tal3 > max); {
-			tal3 = max;
+		if (tal3 > max) {
+			max = tal3;
 		}
-		if (tal3 < min); {
-			tal3 = min;
+		if (tal3 < min) {
+			min = tal3;
 		}
 		
 		int tal4 = input.nextInt();
-		if (tal4 > max); {
-			tal4 = max;
+		if (tal4 > max) {
+			max = tal4;
 		}
-		if (tal4 < min); {
-			tal4 = min;
+		if (tal4 < min) {
+			min = tal4;
 		}
 		
 		int tal5 = input.nextInt();
-		if (tal5 > max); {
-			tal5 = max;
+		if (tal5 > max) {
+			max = tal5;
 		}
-		if (tal5 < min); {
-			tal5 = min;
+		if (tal5 < min) {
+			min = tal5;
 		}
 		
 		int tal6 = input.nextInt();
-		if (tal6 > max); {
-			tal6 = max;
+		if (tal6 > max) {
+			max = tal6;
 		}
-		if (tal6 < min); {
-			tal6 = min;
+		if (tal6 < min) {
+			min = tal6;
 		}
 		
 		int tal7 = input.nextInt();
-		if (tal7> max); {
-			tal7 = max;
+		if (tal7 > max) {
+			max = tal7;
 		}
-		if (tal7 < min); {
-			tal7 = min;
+		if (tal7 < min) {
+			min = tal7;
 		}
 		
 		int tal8 = input.nextInt();
-		if (tal8 > max); {
-			tal8 = max;
+		if (tal8 > max) {
+			max = tal8;
 		}
-		if (tal8 < min); {
-			tal8 = min;
+		if (tal8 < min) {
+			min = tal8;
 		}
 		
 		int tal9 = input.nextInt();
-		if (tal9 > max); {
-			tal9 = max;
+		if (tal9 > max) {
+			max = tal9;
 		}
-		if (tal9 < min); {
-			tal9 = min;
+		if (tal9 < min) {
+			min = tal9;
 		}
 		
 		int tal10 = input.nextInt();
-		if (tal10 > max); {
-			tal10 = max;
+		if (tal10 > max) {
+			max = tal10;
 		}
-		if (tal10 < min); {
-			tal10 = min;
+		if (tal10 < min) {
+			min = tal10;
 		}
 		
+		double summa = tal1 + tal2 + tal3 + tal4 + tal5 + tal6 + tal7 + tal8 + tal9 + tal10;
+		double medel = summa / 10;
 		
+		System.out.println("Högst: " + max);
+		System.out.println("Minst: " + min);
+		System.out.println("Medel: " + medel);
 		
+		input.close();
 	}
 
 }
